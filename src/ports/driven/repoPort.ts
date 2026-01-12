9f51ef79bc864e3f6abbea1119891e49b17e1a2e
Lwiisport <louis.gallo10@gmail.com>
2026-01-12 16:56:42 +0100

    ajout fichier "ports" et "services"

--- src/ports/driven/repoPort.ts
+++ src/ports/driven/repoPort.ts
@@ -1,7 +1,21 @@
 import { Address } from '../../domain/address';
+import { CivilStatus } from '../../domain/civilStatus';
+import { ProfessionalData } from '../../domain/professionalData';
 
 export interface AddressRepositoryPort {
   findAll(): Promise<Address[]>;
   findById(id: string): Promise<Address | null>;
   save(address: Omit<Address, 'id'>): Promise<Address>;
+}
+
+export interface CivilStatusRepositoryPort {
+  findAll(): Promise<CivilStatus[]>;
+  findById(id: string): Promise<CivilStatus | null>;
+  save(civil_status: Omit<CivilStatus, 'id'>): Promise<CivilStatus>;
+}
+
+export interface ProfessionalDataRepositoryPort {
+  findAll(): Promise<ProfessionalData[]>;
+  findById(id: string): Promise<ProfessionalData | null>;
+  save(professional_data: Omit<ProfessionalData, 'id'>): Promise<ProfessionalData>;
 }
