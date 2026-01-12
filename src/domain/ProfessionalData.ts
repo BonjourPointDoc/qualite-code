6e58d1fef18a805cc69dcc621165b90f698b5fb1
Lwiisport <louis.gallo10@gmail.com>
2026-01-12 16:39:50 +0100

    ajout classes

---
+++ src/domain/ProfessionalData.ts
@@ -0,0 +1,23 @@
+export class ProfessionalData {
+    id?: number;
+    civil_status_id?: number;
+    situation: string;
+    company: string;
+    salary: string;
+    contract: string;
+    hours_per_day: number;
+    overtime: number;
+    paid_overtime: number;
+
+    constructor(civil_status_id: number, situation: string, company: string, salary: string, contract: string, hours_per_day: number, overtime: number, paid_overtime: number, id?: number) {
+        this.id = id;
+        this.civil_status_id = civil_status_id;
+        this.situation = situation;
+        this.company = company;
+        this.salary = salary;
+        this.contract = contract;
+        this.hours_per_day = hours_per_day;
+        this.overtime = overtime;
+        this.paid_overtime = paid_overtime;
+    }
+}
