6748586859c1cd95aae345d9e1c56a32011e217d
Lwiisport <louis.gallo10@gmail.com>
2026-01-14 10:52:35 +0100

    Ajout test controller.

---
+++ src/adapters/driving/professionalDataController.test.ts
@@ -0,0 +1,108 @@
+import request from 'supertest';
+import express from 'express';
+import router from './professionalDataController';
+import { ProfessionalDataService } from '../../services/professionalDataService';
+import {ProfessionalData} from "../../domain/professionalData";
+
+jest.mock('../../services/professionalDataService');
+
+const app = express();
+app.use(express.json());
+app.use('/pro-data', router);
+
+describe ('ProfessionalDataController', () => {
+    const mockService = ProfessionalDataService as jest.MockedClass<typeof ProfessionalDataService>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    describe('Get /pro-data', () => {
+        it('should return a list of professional data', async () => {
+            const sample: ProfessionalData[] = [new ProfessionalData(0, "Engineer", "Capgemini", "2300", "CDI", 7, 0, 0),
+                                                new ProfessionalData(0, "Taxi Driver", "Uber", "1500", "CDI", 7, 0, 0)]
+            mockService.prototype.listProfessionalData.mockResolvedValue(sample);
+
+            const res = await request(app).get('/pro-data');
+
+            expect(res.status).toEqual(200);
+            expect(res.body).toEqual(sample);
+        });
+    });
+
+    describe('GET /pro-data/:id', () => {
+        it('should return professional data if found', async () => {
+            const sample: ProfessionalData[] = [new ProfessionalData(0, "Engineer", "Capgemini", "2300", "CDI", 7, 0, 0, 0),
+                                                new ProfessionalData(0, "Taxi Driver", "Uber", "1500", "CDI", 7, 0, 0, 1)]
+            mockService.prototype.getProfessionalData.mockResolvedValue(sample[0]);
+
+            const res = await request(app).get('/pro-data/0');
+
+            expect(res.status).toEqual(200);
+            expect(res.body).toEqual(sample[0]);
+        });
+
+        it('should return 404 if not found', async () => {
+            mockService.prototype.getProfessionalData.mockResolvedValue(null);
+
+            const res = await request(app).get('/pro-data/99');
+
+            expect(res.status).toEqual(404);
+        });
+    });
+
+    describe('POST /pro-data', () => {
+       it('should create professional data', async () => {
+           const input = new ProfessionalData(0, "Engineer", "Capgemini", "2300", "CDI", 7, 0, 0, 0);
+           mockService.prototype.createProfessionalData.mockResolvedValue(input);
+           const res = await request(app).post('/pro-data').send(input);
+
+           expect(res.status).toBe(201);
+           expect(res.body).toEqual(input);
+       });
+
+        it('should return 400 if missing fields', async () => {
+            const res = await request(app).post('/pro-data').send({company: 'Capgemini'});
+
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('PUT /pro-data', () => {
+        it('should update professional data', async () => {
+            mockService.prototype.updateProfessionalData.mockResolvedValue({id:0, company: 'Updated Corp'} as any);
+
+           const res = await request(app).put('/pro-data').send({
+               civil_status_id: 1,
+               situation: 'Employee',
+               company: 'Updated Corp',
+               salary: 3500,
+               contract: 'CDI',
+               hours_per_day: 8,
+               overtime: false,
+               paid_overtime: false,
+               id:0
+           });
+
+           expect(res.status).toBe(201);
+           expect(res.body.company).toBe('Updated Corp');
+        });
+    });
+
+    describe('DELETE /pro-data/:id', () => {
+       it('should delete professional data', async () => {
+           mockService.prototype.deleteProfessionalData.mockResolvedValue({id: 1} as any);
+
+           const res = await request(app).delete('/pro-data/1');
+
+           expect(res.status).toBe(200);
+       })
+
+        it('should return 404 if not found', async () => {
+            mockService.prototype.deleteProfessionalData.mockResolvedValue(null);
+            const res = await request(app).delete('/pro-data/99');
+
+            expect(res.status).toBe(404);
+        })
+    });
+})
--- src/adapters/driving/professionalDataController.ts
+++ src/adapters/driving/professionalDataController.ts
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const { civil_status_id, situation, company, salary, contract, hours_per_day, overtime, paid_overtime  } = req.body;
-    if (!civil_status_id || !situation || !company || !salary || !contract || !hours_per_day || !overtime || !paid_overtime) {
+    if (civil_status_id === undefined || situation === undefined  || company === undefined  || salary === undefined  || contract === undefined  || hours_per_day === undefined || overtime === undefined  || paid_overtime === undefined ) {
         return res.status(400).json({ message: ' civil_status_id, situation, company, salary, contract, hours_per_day, overtime and paid_overtime required' });
     }
     const created = await service.createProfessionalData(new ProfessionalData( civil_status_id, situation, company, salary, contract, hours_per_day, overtime, paid_overtime));
@@ -24,7 +24,7 @@ router.post('/', async (req, res) => {
 
 router.put('/', async (req, res) => {
     const { civil_status_id, situation, company, salary, contract, hours_per_day, overtime, paid_overtime  } = req.body;
-    if (!civil_status_id || !situation || !company || !salary || !contract || !hours_per_day || !overtime || !paid_overtime) {
+    if (civil_status_id === undefined || situation === undefined || company === undefined || salary === undefined || contract === undefined || hours_per_day === undefined || overtime === undefined || paid_overtime === undefined) {
         return res.status(400).json({ message: ' civil_status_id, situation, company, salary, contract, hours_per_day, overtime and paid_overtime required' });
     }
     const updated = await service.updateProfessionalData(new ProfessionalData(civil_status_id, situation, company, salary, contract, hours_per_day, overtime, paid_overtime));
@@ -34,14 +34,16 @@ router.put('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const id = Number.parseInt(req.params.id);
     const found = await service.deleteProfessionalData(id);
-    if (!found) return res.status(404).json({ message: 'Not found' });
+    if (found === null) return res.status(404).json({ message: 'Not found' });
     res.json(found);
 });
 
 router.get('/:id', async (req, res) => {
     const id = Number.parseInt(req.params.id);
     const found = await service.getProfessionalData(id);
-    if (!found) return res.status(404).json({ message: 'Not found' });
+    if (found === null) {
+        return res.status(404).json({ message: 'Not found' });
+    }
     res.json(found);
 });
 
